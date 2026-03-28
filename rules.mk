803ce3e86b77163c39c3ccd6620a98e6bb80e1a6
JeromeT <jerome.tengg@gmx.de>
2026-03-28 21:14:35 +0100

    switch to json format

--- rules.mk
+++ rules.mk
@@ -1,10 +1,15 @@
-VIA_ENABLE = no
-VIAL_ENABLE = no
-LTO_ENABLE = yes
-RAW_ENABLE = no
+# Nur das Nötigste erlauben
+LTO_ENABLE = yes          # Macht die Firmware kleiner und stabiler
+VIA_ENABLE = no           # SICHERHEIT: Keine Konfiguration ohne Flashen
+VIAL_ENABLE = no          # SICHERHEIT: Keine Konfiguration ohne Flashen
+MOUSEKEY_ENABLE = no      # SICHERHEIT: Nur als Tastatur ausgeben, nicht als Maus
+RGB_MATRIX_ENABLE = no    # Strom sparen und Fehlerquellen minimieren
+RGBLIGHT_ENABLE = no      # Strom sparen und Fehlerquellen minimieren
+CONSOLE_ENABLE = no       # SICHERHEIT: Kein Debug-Logging über USB
+COMMAND_ENABLE = no       # Keine Bootmagic-Befehle
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = no
+MOUSEKEY_ENABLE = no
 CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-
-
-# Wichtig für die Kommunikation der Corne v4 (RP2040)
-SERIAL_DRIVER = vendor
+VIA_ENABLE = no
+LTO_ENABLE = yes
