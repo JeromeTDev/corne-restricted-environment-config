6de873a1b249938d618e63918735c958a620588b
JeromeT <jerome.tengg@gmx.de>
2026-03-30 17:11:40 +0200

    add layer 2

--- keymap.c
+++ keymap.c
@@ -17,6 +17,7 @@ const uint16_t PROGMEM combo_rightBR[] = {KC_N, KC_M, COMBO_END};    // N + M = 
 const uint16_t PROGMEM combo_backslash[] = {KC_DOT, KC_SLSH, COMBO_END};
 const uint16_t PROGMEM combo_pipe[] = {KC_SLSH, KC_RSFT, COMBO_END};      // / + ShiftR = |
 
+
 const uint16_t PROGMEM combo_tilde[] = {KC_L, KC_SCLN, COMBO_END};    // L + ; = ~
 const uint16_t PROGMEM combo_Grave[] = {KC_SCLN, KC_QUOT, COMBO_END};    // ; + ' = ` 
 
@@ -48,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_PSCR, KC_VOLU, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
         LCTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_LALT, KC_VOLD, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-        KC_LGUI, LT(1,KC_SPC), KC_ENT, KC_ENT, KC_SPC, KC_RALT
+        KC_LGUI, LT(1,KC_SPC), KC_ENT, KC_ENT,LT(2,KC_SPC), KC_RALT
     ),
     [1] = LAYOUT_split_3x6_3_ex2(
         KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_TRNS, KC_TRNS, KC_PSLS, KC_7, KC_8, KC_9, KC_PLUS, KC_TRNS, 
